e23c6a6fa939d3ba72b6f07eb6646efaf4a27cf9
Roger Chappel <miscanalysis@gmail.com>
2026-08-18 10:08:39 +1000

    fix: recognize passive prohibition wording

--- src/index.js
+++ src/index.js
@@ -144,7 +144,8 @@ function hasAffirmativeRisk(line, pattern) {
   return clauses.some((clause) => {
     if (!pattern.test(clause)) return false;
     return !/\b(?:do\s+not|don't|never|must(?:\s+not|n't)|should(?:\s+not|n't)|cannot|can't|no)\b/i.test(clause)
-      && !/\b(?:prohibited|forbidden|disallowed)\b/i.test(clause);
+      && !/\b(?:prohibited|forbidden|disallowed)\b/i.test(clause)
+      && !/\b(?:is|are|was|were|be|being|been)\s+(?:not|n't)\s+(?:allowed|permitted)\b/i.test(clause);
   });
 }
 
